8902be8b766a8743cf45d92a3383d7d4d79dc3d5
Muhammad Nabil Rabbani <muhammadrabbani463@smk.belajar.id>
2023-05-09 08:50:35 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="" rel="stylesheet"/>
+    <title>Belajar PHP</title>
+  </head>
+  <body>
+
+    <?php
+        // tahap 1
+        echo "Belajar PHP";
+        echo "\n";
+
+        // tahap 2
+        $name = "Belajar PHP menggunakan var";
+        echo $name;
+    ?>
+
+  </body>
+</html>
---
+++ project.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="" rel="stylesheet"/>
+    <title>Belajar PHP</title>
+  </head>
+  <body>
+
+    <?php
+        echo "Belajar PHP - Project";
+        
+    ?>
+
+  </body>
+</html>
